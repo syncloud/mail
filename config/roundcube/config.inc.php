a3fef8b1e79e52d31dd836e4da59db7dca98ed9b
Boris Rybalkin <ribalkin@gmail.com>
2016-03-09 10:03:48 +0000

    fix roundcube config

--- config/roundcube/config.inc.php
+++ config/roundcube/config.inc.php
@@ -1,5 +1,6 @@
 $config['db_dsnw'] = 'pgsql://mail:@unix(/opt/data/mail/postgresql)/mail';
-$config['default_host'] = 'localhost';$config['smtp_server'] = 'localhost';
+$config['default_host'] = 'localhost';
+$config['smtp_server'] = 'localhost';
 $config['support_url'] = '';
 $config['des_key'] = '46adfbf655e235b60f56f881';
 $config['plugins'] = array();
