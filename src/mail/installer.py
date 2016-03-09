11cacbec21a9f3ca49d6765f2f4010cc6251352f
Boris Rybalkin <ribalkin@gmail.com>
2016-03-09 10:47:30 +0000

    fix config

--- src/mail/installer.py
+++ src/mail/installer.py
@@ -108,7 +108,7 @@ class MailInstaller:
     def generate_roundcube_config(self):
         shutil.copyfile(self.config.roundcube_config_file_template(), self.config.roundcube_config_file())
         with open(self.config.roundcube_config_file(), "a") as config_file:
-            config_file.write('$config['default_host'] = '{0}';\n'.format(self.app_domain))
+            config_file.write("$config['default_host'] = '{0}';\n".format(self.app_domain))
 
     def generate_postfix_config(self):
         
