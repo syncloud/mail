03c79576037e25119a9396ec2fab71c43f0e98ef
Boris Rybalkin <ribalkin@gmail.com>
2016-03-12 13:44:01 +0000

    fix test

--- integration/verify.py
+++ integration/verify.py
@@ -120,14 +120,13 @@ def test_postfix_auth():
     server.login(DEVICE_USER, DEVICE_PASSWORD)
 
 def test_postfix_submission():
-    server = smtplib.SMTP('localhos:587', timeout=10)
+    server = smtplib.SMTP('localhost:587', timeout=10)
     server.set_debuglevel(1)
     server.ehlo()
     server.starttls()
     #server.sendmail(fromaddr, toaddrs, msg)
     server.quit()
-
-   server.login(DEVICE_USER, DEVICE_PASSWORD)
+    server.login(DEVICE_USER, DEVICE_PASSWORD)
 
 def test_postfix_ldap_aliases(user_domain):
     run_ssh('/opt/app/mail/postfix/usr/sbin/postmap -q {0}@{1} ldap:/opt/app/mail/config/postfix/ldap-aliases.cf'
