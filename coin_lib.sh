847153166b96cdc6d66d55f9c299db74f62adb43
Boris Rybalkin <ribalkin@gmail.com>
2016-03-18 05:11:12 +0000

    adding pytz library

--- coin_lib.sh
+++ coin_lib.sh
@@ -19,5 +19,6 @@ cd lib
 coin py https://pypi.python.org/packages/2.7/r/requests/requests-2.7.0-py2.py3-none-any.whl
 coin py https://pypi.python.org/packages/source/s/syncloud-lib/syncloud-lib-2.tar.gz
 coin py https://pypi.python.org/packages/source/t/tzlocal/tzlocal-1.2.2.tar.gz
+coin py https://pypi.python.org/packages/source/p/pytz/pytz-2016.1.tar.gz
 
 cd ..
