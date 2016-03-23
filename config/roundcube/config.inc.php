587f04f610e9dea049cbaaf1952ae184082df0b0
Boris Rybalkin <ribalkin@gmail.com>
2016-03-23 08:13:40 +0000

    allow self-signed certificate

--- config/roundcube/config.inc.php
+++ config/roundcube/config.inc.php
@@ -18,3 +18,12 @@ $config['smtp_pass'] = '%p';
 $config['plugins'] = array();
 
 $config['default_host'] = 'tls://localhost';
+
+$config['imap_conn_options'] = array(
+  'ssl' => array(
+      'verify_peer' => true,
+      'allow_self_signed' => true,
+      'verify_depth' => 3,
+       'ciphers' => 'TLSv1+HIGH:!aNull:@STRENGTH',
+       'cafile' => '/opt/data/platform/syncloud.crt', ), 
+    );
