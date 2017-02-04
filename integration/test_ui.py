b7397835b4e08b469418d4f76aa08531a5403657
Boris Rybalkin <ribalkin@gmail.com>
2017-02-04 23:55:50 +0000

    fix and debug waiting for the main page

--- integration/test_ui.py
+++ integration/test_ui.py
@@ -52,12 +52,14 @@ def test_web_with_selenium(user_domain, device_domain):
 
     time.sleep(10)
 
-   driver.get_screenshot_as_file(join(screenshot_dir, 'main.png'))
+    driver.get_screenshot_as_file(join(screenshot_dir, 'main.png'))
 
-    print(driver.page_source.encode("utf-8"))
+    page = driver.page_source.encode("utf-8")
+    print(page)
 
     username = '{0}@{1}'.format(DEVICE_USER, device_domain)
-    wait_driver.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, '.username'), username))
+   print('found: {0}'.format(username in page))
+ wait_driver.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, '.username'), username))
 
 
 
