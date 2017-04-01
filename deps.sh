4e3ae51de76bcfe48d6460a4cafa96b828a77ec3
Boris Rybalkin <ribalkin@gmail.com>
2017-04-01 22:38:33 +0100

    fix docker ports

--- deps.sh
+++ deps.sh
@@ -6,11 +6,11 @@ python get-pip.py
 pip install coin
 ARCH=$(uname -m)
 if [ ${ARCH} == "x86_64" ]; then
-  wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+  wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2 --progress dot:giga
   tar xjvf phantomjs-2.1.1-linux-x86_64.tar.bz2
   cp ./phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/bin
 else
-  wget https://github.com/fg2it/phantomjs-on-raspberry/releases/download/v2.1.1-wheezy-jessie-armv6/phantomjs
+  wget https://github.com/fg2it/phantomjs-on-raspberry/releases/download/v2.1.1-wheezy-jessie-armv6/phantomjs --progress dot:giga
   cp phantomjs /usr/bin
 fi
 chmod +x /usr/bin/phantomjs
