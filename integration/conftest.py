a25777790f3c06937e2f8bf81b7fffe635c2a18c
Boris Rybalkin <ribalkin@gmail.com>
2017-06-19 19:39:24 +0100

    fix test config

--- integration/conftest.py
+++ integration/conftest.py
@@ -1,13 +1,17 @@
 import pytest
 
-
 SYNCLOUD_INFO = 'syncloud.info'
+DEVICE_USER = 'user'
+DEVICE_PASSWORD = 'password'
+
 
 def pytest_addoption(parser):
     parser.addoption("--email", action="store")
     parser.addoption("--password", action="store")
     parser.addoption("--domain", action="store")
     parser.addoption("--release", action="store")
+    parser.addoption("--installer", action="store")
+    parser.addoption("--device-host", action="store")
     parser.addoption("--app-archive-path", action="store")
 
 
@@ -17,24 +21,22 @@ def auth(request):
     return config.getoption("--email"), \
            config.getoption("--password"), \
            config.getoption("--domain"), \
-           config.getoption("--release"), \
-           config.getoption("--app-archive-path")
+           config.getoption("--release")
 
 
-@pytest.fixture(scope='module')
-def device_domain(auth):
-    _, _, domain, _, _ = auth
-    return '{0}.{1}'.format(domain, SYNCLOUD_INFO)
+@pytest.fixture(scope='session')
+def user_domain(request):
+    return 'mail.{0}.{1}'.format(request.config.getoption("--domain"), SYNCLOUD_INFO)
 
 
-@pytest.fixture(scope='module')
-def user_domain(device_domain):
-    return 'mail.{0}'.format(device_domain)
+@pytest.fixture(scope='session')
+def app_archive_path(request):
+    return request.config.getoption("--app-archive-path")
 
+@pytest.fixture(scope='session')
+def installer(request):
+    return request.config.getoption("--installer")
 
-@pytest.fixture(scope='module')
-def app_archive_path(auth):
-    _, _, _, _, app_archive = auth
-    return app_archive
-
-
+@pytest.fixture(scope='session')
+def device_host(request):
+    return request.config.getoption("--device-host")
