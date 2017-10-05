a6df797df43a5801834badd727cab70457b2241f
Boris Rybalkin <ribalkin@gmail.com>
2017-10-05 22:49:50 +0100

    lazy config

--- src/mail/config.py
+++ src/mail/config.py
@@ -7,49 +7,62 @@ class Config:
     def __init__(self, config_dir):
         filename = join(config_dir, 'mail.cfg')
         self.parser = ConfigParser()
-        self.parser.read(filename)
         self.filename = filename
 
     def app_name(self):
+        self.parser.read(self.filename)
         return self.parser.get('mail', 'app_name')
 
     def install_path(self):
+        self.parser.read(self.filename)
         return self.parser.get('mail', 'install_path')
 
     def data_dir(self):
+        self.parser.read(self.filename)
         return self.parser.get('mail', 'data_dir')
 
     def bin_dir(self):
+        self.parser.read(self.filename)
         return self.parser.get('mail', 'bin_dir')
 
     def root_path(self):
+        self.parser.read(self.filename)
         return self.parser.get('mail', 'root_path')
 
     def port(self):
+        self.parser.read(self.filename)
         return self.parser.getint('mail', 'port')
 
     def psql(self):
+        self.parser.read(self.filename)
         return self.parser.get('mail', 'psql')
 
     def db_socket(self):
+        self.parser.read(self.filename)
         return self.parser.get('mail', 'db_socket')
 
     def postfix_main_config_file(self):
+        self.parser.read(self.filename)
         return self.parser.get('mail', 'postfix_main_config_file')
 
     def roundcube_config_file_template(self):
+        self.parser.read(self.filename)
         return self.parser.get('mail', 'roundcube_config_file_template')
 
     def roundcube_config_file(self):
+        self.parser.read(self.filename)
         return self.parser.get('mail', 'roundcube_config_file')
 
     def dovecot_config_file(self):
+        self.parser.read(self.filename)
         return self.parser.get('mail', 'dovecot_config_file')
 
     def db_init_file(self):
+        self.parser.read(self.filename)
         return self.parser.get('mail', 'db_init_file')
 
     def php_ini(self):
+        self.parser.read(self.filename)
         return self.parser.get('mail', 'php_ini')
 
 
