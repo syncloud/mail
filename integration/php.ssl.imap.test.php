1c7fc6321412d032d69a1086f3125fe05227e79e
Boris Rybalkin <ribalkin@gmail.com>
2017-12-14 20:28:09 +0000

    fix php socket

--- integration/php.ssl.imap.test.php
+++ integration/php.ssl.imap.test.php
@@ -3,7 +3,7 @@
 require('/config.inc.php');
 $stream_context = stream_context_create($config['imap_conn_options']);
 
-$fp = stream_socket_client($config['default_host'],
+$fp = stream_socket_client($config['default_host'] . ':143',
    $errno, $errstr, 30, STREAM_CLIENT_CONNECT, $stream_context);
 fwrite($fp, "A Logout\n");
 while($line = fgets($fp, 8192)) echo $line;
