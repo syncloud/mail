7866a68233dca2eda2f71d1e199b4202d7507c9e
Boris Rybalkin <ribalkin@gmail.com>
2017-12-16 09:55:03 +0000

    try ssl protocol

--- integration/php.ssl.imap.test.php
+++ integration/php.ssl.imap.test.php
@@ -3,7 +3,7 @@
 require('/config.inc.php');
 $stream_context = stream_context_create($config['imap_conn_options']);
 
-$socket = stream_socket_client($config['default_host'] . ':143',
+$socket = stream_socket_client('ssl://localhost:143',
    $errno, $errstr, 30, STREAM_CLIENT_CONNECT, $stream_context);
 if ($socket) {
     fwrite($socket, "A Logout\n");
