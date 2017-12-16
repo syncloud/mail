6708613835ca23d9f9e86cd02b232e65d878c5f6
Boris Rybalkin <ribalkin@gmail.com>
2017-12-16 09:10:56 +0000

    fix

--- integration/php.ssl.imap.test.php
+++ integration/php.ssl.imap.test.php
@@ -3,7 +3,13 @@
 require('/config.inc.php');
 $stream_context = stream_context_create($config['imap_conn_options']);
 
-$fp = stream_socket_client($config['default_host'] . ':143',
+$socket = stream_socket_client($config['default_host'] . ':143',
    $errno, $errstr, 30, STREAM_CLIENT_CONNECT, $stream_context);
-fwrite($fp, "A Logout\n");
-while($line = fgets($fp, 8192)) echo $line;
+if ($socket) {
+    fwrite($socket, "A Logout\n");
+    while($line = fgets($socket, 8192))
+        echo $line;
+} else {
+    echo "Failure $errno errstr $errstr.\n";
+}
+
