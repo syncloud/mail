0fc26438095053ab52519af5d62487d6d68fe15a
Boris Rybalkin <ribalkin@gmail.com>
2017-12-17 10:45:01 +0000

    fix test device dns

--- integration/test-docker.sh
+++ integration/test-docker.sh
@@ -75,6 +75,7 @@ curl https://raw.githubusercontent.com/mguillem/JSErrorCollector/master/dist/JSE
 
 #fix dns
 device_ip=$(getent hosts ${DEVICE_HOST} | awk '{ print $1 }')
+echo "$device_ip $DOMAIN.syncloud.info" >> /etc/hosts
 echo "$device_ip $APP.$DOMAIN.syncloud.info" >> /etc/hosts
 
 cat /etc/hosts
