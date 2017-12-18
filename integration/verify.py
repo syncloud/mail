9a27e03928bdb1535e566b3e836d266e1bd8cb50
Boris Rybalkin <ribalkin@gmail.com>
2017-12-18 20:04:29 +0000

    try redirect domain

--- integration/verify.py
+++ integration/verify.py
@@ -90,10 +90,17 @@ def test_start(module_setup):
 def test_activate_device(auth, device_domain):
     email, password, domain, release = auth
 
-    response = requests.post('http://{0}:81/rest/activate_custom_domain'.format(device_domain),
-                             data={'full_domain': device_domain, 
-                                   'device_username': DEVICE_USER, 
+#    response = requests.post('http://{0}:81/rest/activate_custom_domain'.format(device_domain),
+#                             data={'full_domain': device_domain, 
+#                                   'device_username': DEVICE_USER, 
+#                                   'device_password': DEVICE_PASSWORD})
+                                   
+    response = requests.post('http://{0}:81/rest/activate'.format(user_domain),
+                             data={'main_domain': SYNCLOUD_INFO,
+                                   'redirect_email': email, 'redirect_password': password,
+                                   'user_domain': domain, 'device_username': DEVICE_USER,
                                    'device_password': DEVICE_PASSWORD})
+                                   
     assert response.status_code == 200, response.text
     global LOGS_SSH_PASSWORD
     LOGS_SSH_PASSWORD = DEVICE_PASSWORD
