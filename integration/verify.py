b4e7c52996abe5a43f3f06d8535bffe7c15537a6
Boris Rybalkin <ribalkin@gmail.com>
2017-12-18 22:12:24 +0000

    fix activate

--- integration/verify.py
+++ integration/verify.py
@@ -95,7 +95,7 @@ def test_activate_device(auth, device_domain):
 #                                   'device_username': DEVICE_USER, 
 #                                   'device_password': DEVICE_PASSWORD})
                                    
-    response = requests.post('http://{0}:81/rest/activate'.format(user_domain),
+    response = requests.post('http://{0}:81/rest/activate'.format(device_domain),
                              data={'main_domain': SYNCLOUD_INFO,
                                    'redirect_email': email, 'redirect_password': password,
                                    'user_domain': domain, 'device_username': DEVICE_USER,
