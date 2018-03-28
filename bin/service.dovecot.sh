c8434d0dc049fed01f16e2ce08dc2c43696d3655
Boris Rybalkin <ribalkin@gmail.com>
2018-03-28 09:21:42 +0100

    dovecote service wrapper

--- bin/service.dovecot.sh
+++ bin/service.dovecot.sh
@@ -3,18 +3,18 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
 if [[ -z "$1" ]]; then
-    echo "usage $0 [start]"
+    echo "usage $0 [action]"
     exit 1
 fi
 
-export LD_LIBRARY_PATH=${DIR}/php/lib:${DIR}/postgresql/lib
+export LD_LIBRARY_PATH=${DIR}/dovecot/lib/dovecot
 
 case $1 in
-start)
-    exec $DIR/php/sbin/php-fpm -y ${SNAP_COMMON}/config/php/php-fpm.conf -c ${SNAP_COMMON}/config/php/php.ini
+pre-start)
+    /bin/rm -rf ${SNAP_COMMON}/dovecot/master.pid
     ;;
-post-start)
-    timeout 5 /bin/bash -c 'until [ -S '${SNAP_COMMON}'/log/php5-fpm.sock ]; do echo "waiting for ${SNAP_COMMON}/log/php5-fpm.sock"; sleep 1; done'
+start)
+    exec $DIR/dovecot/sbin/dovecot -F -c ${SNAP_COMMON}/config/dovecot/dovecot.conf
     ;;
 *)
     echo "not valid command"
