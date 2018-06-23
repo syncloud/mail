96b0d3c9498dd7d30a68dc17c60f39d0e82a9a79
Boris Rybalkin <ribalkin@gmail.com>
2018-06-23 10:06:59 +0100

    postfix config check

--- integration/verify.py
+++ integration/verify.py
@@ -144,7 +144,10 @@ def test_running_roundcube(user_domain):
 
 def test_postfix_status(user_domain, app_dir, data_dir):
     run_ssh(user_domain, '{0}/postfix/usr/sbin/postfix.sh -c {1}/config/postfix -v status > {1}/log/postfix.status.log 2>&1'.format(app_dir, data_dir), password=LOGS_SSH_PASSWORD, throw=False)
-   
+
+def test_postfix_check(user_domain, app_dir, data_dir):
+    run_ssh(user_domain, '{0}/postfix/usr/sbin/postfix.sh -c {1}/config/postfix -v check > {1}/log/postfix.check.log 2>&1'.format(app_dir, data_dir), password=LOGS_SSH_PASSWORD, throw=False)
+
 def test_dovecot_auth(user_domain, app_dir, data_dir):
     run_ssh(user_domain,
             '{0}/dovecot/bin/doveadm -D -c {1}/config/dovecot/dovecot.conf auth test {2} {3}'
