85fc327b0f26ee19c1178710eec5546b572c7962
Boris Rybalkin <ribalkin@gmail.com>
2018-07-12 07:23:18 +0100

    mobile screenshots

--- integration/test-ui.py
+++ integration/test-ui.py
@@ -48,35 +48,45 @@ def driver():
 
 def test_web_with_selenium(driver, user_domain, device_domain):
 
-    wait_driver = WebDriverWait(driver, 60)
-
-    screenshot_dir = join(DIR, 'screenshot')
-    if exists(screenshot_dir):
-        shutil.rmtree(screenshot_dir)
-    os.mkdir(screenshot_dir)
-
     driver.get("https://{0}".format(user_domain))
     
     time.sleep(2)
-    driver.get_screenshot_as_file(join(screenshot_dir, 'login.png'))
-
-    print(driver.page_source.encode("utf-8"))
+    screenshots(driver, screenshot_dir, 'login')
 
     user = driver.find_element_by_id("rcmloginuser")
     user.send_keys(DEVICE_USER)
     password = driver.find_element_by_id("rcmloginpwd")
     password.send_keys(DEVICE_PASSWORD)
-    driver.get_screenshot_as_file(join(screenshot_dir, 'login.png'))
+   
+    screenshots(driver, screenshot_dir, 'login-filled')
+  
     password.send_keys(Keys.RETURN)
 
     time.sleep(10)
-    driver.get_screenshot_as_file(join(screenshot_dir, 'login_progress.png')) 
+    screenshots(driver, screenshot_dir, 'login_progress')
 
+    wait_driver = WebDriverWait(driver, 60)
     username = '{0}@{1}'.format(DEVICE_USER, device_domain)
-    #print('found: {0}'.format(username in page))
     wait_driver.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, '.username'), username))
     time.sleep(10)
-    driver.get_screenshot_as_file(join(screenshot_dir, 'main.png'))
+    
+    screenshots(driver, screenshot_dir, 'main')
+    
 
-    page = driver.page_source.encode("utf-8")
-    print(page)
+def screenshots(driver, dir, name):
+    desktop_w = 1024
+    desktop_h = 768
+    driver.set_window_position(0, 0)
+    driver.set_window_size(desktop_w, desktop_h)
+
+    driver.get_screenshot_as_file(join(dir, '{}.png'.format(name)))
+
+    mobile_w = 400
+    mobile_h = 2000
+    driver.set_window_position(0, 0)
+    driver.set_window_size(mobile_w, mobile_h)
+    driver.get_screenshot_as_file(join(dir, '{}-mobile.png'.format(name)))
+    
+    with open(join(dir, '{0}.html.log'.format(name)), "w") as f:
+        f.write(driver.page_source.encode("utf-8"))
+   
