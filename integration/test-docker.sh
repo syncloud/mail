cf0a2069b1d367032d841da82ea7646803e58cc9
Boris Rybalkin <ribalkin@gmail.com>
2018-08-19 15:01:48 +0100

    remove sam support

--- integration/test-docker.sh
+++ integration/test-docker.sh
@@ -7,42 +7,33 @@ if [ "$#" -lt 7 ]; then
     exit 1
 fi
 
-DOMAIN=$3
+ARCH=$(uname -m)
 VERSION=$4
 RELEASE=$5
 INSTALLER=$6
+DOMAIN=$3-${ARCH}-${DRONE_BRANCH}
 DEVICE_HOST=$7
 
 APP=mail
 GECKODRIVER=0.14.0
 FIREFOX=52.0
-ARCH=$(uname -m)
-
 
 apt install -y expect telnet
 
 if [ $ARCH == "x86_64" ]; then
+    TEST_SUITE="verify.py test-ui.py"
     SNAP_ARCH=amd64
 else
+    TEST_SUITE=verify.py
     SNAP_ARCH=armhf
 fi
 
-if [ $INSTALLER == "snapd" ]; then
-    ARCHIVE=${APP}_${VERSION}_${SNAP_ARCH}.snap
-else
-    ARCHIVE=${APP}-${VERSION}-${ARCH}.tar.gz
-fi
+ARCHIVE=${APP}_${VERSION}_${SNAP_ARCH}.snap
 APP_ARCHIVE_PATH=$(realpath "$ARCHIVE")
 
 cd ${DIR}
 
 echo ${APP_ARCHIVE_PATH}
-
-if [ "$ARCH" == "x86_64" ]; then
-    TEST_SUITE="verify.py test-ui.py"
-else
-    TEST_SUITE=verify.py
-fi
 
 cd ${DIR}
 
@@ -74,10 +65,6 @@ wget http://artifact.syncloud.org/3rdparty/openssl-${ARCH}.tar.gz
 sshpass -p syncloud scp -o StrictHostKeyChecking=no openssl-${ARCH}.tar.gz root@${DEVICE_HOST}:/openssl-${ARCH}.tar.gz
 sshpass -p syncloud ssh -o StrictHostKeyChecking=no root@${DEVICE_HOST} tar xf /openssl-${ARCH}.tar.gz -C /
 sshpass -p syncloud ssh -o StrictHostKeyChecking=no root@${DEVICE_HOST} ls -la /openssl/bin/openssl
-
-coin --to ${DIR} raw --subfolder geckodriver https://github.com/mozilla/geckodriver/releases/download/v${GECKODRIVER}/geckodriver-v${GECKODRIVER}-linux64.tar.gz
-coin --to ${DIR} raw https://ftp.mozilla.org/pub/firefox/releases/${FIREFOX}/linux-x86_64/en-US/firefox-${FIREFOX}.tar.bz2
-curl https://raw.githubusercontent.com/mguillem/JSErrorCollector/master/dist/JSErrorCollector.xpi -o  JSErrorCollector.xpi
 
 #fix dns
 device_ip=$(getent hosts ${DEVICE_HOST} | awk '{ print $1 }')
