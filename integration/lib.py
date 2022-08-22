75579aec328c833f78b0cc56dabb897c230f4307
Boris Rybalkin <ribalkin@gmail.com>
2022-08-22 22:56:25 +0100

    wip

---
+++ integration/lib.py
@@ -0,0 +1,18 @@
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import time
+
+
+def login(selenium, device_user, device_password):
+
+    selenium.open_app()
+    selenium.screenshot('login')
+    selenium.find_by_id("rcmloginuser").send_keys(device_user)
+    password = selenium.find_by_id("rcmloginpwd")
+    password.send_keys(device_password)
+    selenium.screenshot('login-filled')
+    password.send_keys(Keys.RETURN)
+    selenium.screenshot('login_progress')
+    selenium.find_by_xpath("//ul[@id='mailboxlist']")
+    
---
+++ integration/test-upgrade.py
@@ -0,0 +1,37 @@
+import pytest
+from subprocess import check_output
+from syncloudlib.integration.hosts import add_host_alias
+from integration import lib
+from syncloudlib.integration.installer import local_install, wait_for_installer
+from syncloudlib.http import wait_for_rest
+import requests
+TMP_DIR = '/tmp/syncloud'
+
+
+@pytest.fixture(scope="session")
+def module_setup(request, device, artifact_dir):
+    def module_teardown():
+        device.run_ssh('journalctl > {0}/refresh.journalctl.log'.format(TMP_DIR), throw=False)
+        device.scp_from_device('{0}/*'.format(TMP_DIR), artifact_dir)
+        check_output('cp /videos/* {0}'.format(artifact_dir), shell=True)
+        check_output('chmod -R a+r {0}'.format(artifact_dir), shell=True)
+
+    request.addfinalizer(module_teardown)
+
+
+def test_start(module_setup, app, device_host, domain, device):
+    add_host_alias(app, device_host, domain)
+    device.activated()
+    device.run_ssh('rm -rf {0}'.format(TMP_DIR), throw=False)
+    device.run_ssh('mkdir {0}'.format(TMP_DIR), throw=False)
+
+
+def test_upgrade(device, device_user, device_password, device_host, app_archive_path, app_domain):
+    device.run_ssh('snap remove mail')
+    device.run_ssh('snap install mail')
+    local_install(device_host, device_password, app_archive_path)
+    wait_for_rest(requests.session(), "https://{0}".format(app_domain), 200, 10)
+
+
+def test_login(selenium, device_user, device_password):
+    lib.login(selenium, device_user, device_password)
