b5f526b17ace8d68f4cb69d793db4c85b897f37b
Boris Rybalkin <ribalkin@gmail.com>
2022-08-23 18:00:12 +0100

    expect

--- integration/deps.sh
+++ integration/deps.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
 apt-get update
-apt-get install -y sshpass openssh-client netcat rustc file libxml2-dev libxslt-dev build-essential libz-dev curl
+apt-get install -y sshpass openssh-client netcat rustc file libxml2-dev libxslt-dev build-essential libz-dev curl expect
 pip install -r requirements.txt
