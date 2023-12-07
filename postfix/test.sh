93585d19c01e5a937b8be2414f7928b9dbea3850
Boris Rybalkin <ribalkin@gmail.com>
2023-12-07 13:48:01 +0000

    bin

--- postfix/test.sh
+++ postfix/test.sh
@@ -6,7 +6,7 @@ PREFIX=${DIR}/../build/snap/postfix
 du -d10 -h $PREFIX | sort -h | tail -100
 
 ldd ${PREFIX}/usr/sbin/postfix
-
+ls -la ${PREFIX}/bin
 ${PREFIX}/bin/postfix.sh --help || true
 ${PREFIX}/bin/postconf.sh -a
 ${PREFIX}/bin/postconf.sh -A
