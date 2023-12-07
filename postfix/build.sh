82d9c3813cc27d971a17507bd5b36a19d64ab1a6
Boris Rybalkin <ribalkin@gmail.com>
2023-12-07 11:36:26 +0000

    more libs

--- postfix/build.sh
+++ postfix/build.sh
@@ -7,50 +7,50 @@ export TMPDIR=/tmp
 export TMP=/tmp
 NAME=postfix
 VERSION=3.4.28
-OPENSSL_VERSION=1.1.0l
-SASL_VERSION=2.1.28
+#OPENSSL_VERSION=1.1.0l
+#SASL_VERSION=2.1.28
 BUILD_DIR=${DIR}/build
 
 echo "building ${NAME}"
 
 apt update
-apt -y install libdb-dev libldap2-dev libsasl2-dev m4 wget build-essential curl libssl-dev
+apt -y install libdb-dev libldap2-dev libsasl2-dev m4 wget build-essential curl libssl-dev libsasl2-dev
 
 rm -rf ${BUILD_DIR}
 mkdir -p ${BUILD_DIR}
 
-cd ${BUILD_DIR}
-wget https://github.com/cyrusimap/cyrus-sasl/releases/download/cyrus-sasl-${SASL_VERSION}/cyrus-sasl-${SASL_VERSION}.tar.gz
-tar xf cyrus-sasl-${SASL_VERSION}.tar.gz
-cd cyrus-sasl-${SASL_VERSION}
-./configure \
-    --enable-static \
-    --enable-shared \
-    --enable-alwaystrue \
-    --enable-checkapop \
-    --enable-cram \
-    --enable-digest \
-    --enable-otp \
-    --disable-srp \
-    --disable-srp-setpass \
-    --disable-krb4 \
-    --enable-gss_mutexes \
-    --enable-auth-sasldb \
-    --enable-plain \
-    --enable-anon \
-    --enable-login \
-    --enable-ntlm \
-    --disable-passdss \
-    --disable-macos-framework \
-    --with-pam=/usr \
-    --with-saslauthd=/var/snap/mail/common/saslauthd \
-    --with-configdir=/snap/mail/current/postfix/lib/sasl2 \
-    --with-plugindir=/snap/mail/current/postfix/lib/sasl2 \
-    --sysconfdir=/snap/mail/current/postfix/config/sasl2 \
-    --with-devrandom=/dev/urandom \
-    --with-sphinx-build 
-make
-make install
+#cd ${BUILD_DIR}
+#wget https://github.com/cyrusimap/cyrus-sasl/releases/download/cyrus-sasl-${SASL_VERSION}/cyrus-sasl-${SASL_VERSION}.tar.gz
+#tar xf cyrus-sasl-${SASL_VERSION}.tar.gz
+#cd cyrus-sasl-${SASL_VERSION}
+#./configure \
+#    --enable-static \
+#    --enable-shared \
+#    --enable-alwaystrue \
+#    --enable-checkapop \
+#    --enable-cram \
+#    --enable-digest \
+#    --enable-otp \
+#    --disable-srp \
+#    --disable-srp-setpass \
+#    --disable-krb4 \
+#    --enable-gss_mutexes \
+#    --enable-auth-sasldb \
+#    --enable-plain \
+#    --enable-anon \
+#    --enable-login \
+#    --enable-ntlm \
+#    --disable-passdss \
+#    --disable-macos-framework \
+#    --with-pam=/usr \
+#    --with-saslauthd=/var/snap/mail/common/saslauthd \
+#    --with-configdir=/snap/mail/current/postfix/lib/sasl2 \
+#    --with-plugindir=/snap/mail/current/postfix/lib/sasl2 \
+#    --sysconfdir=/snap/mail/current/postfix/config/sasl2 \
+#    --with-devrandom=/dev/urandom \
+#    --with-sphinx-build
+#make
+#make install
 
 #cd ${BUILD_DIR}
 #curl -O https://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz
@@ -73,16 +73,18 @@ export CCARGS='-DDEF_CONFIG_DIR=\"/var/snap/mail/current/config/postfix\" \
   -DUSE_TLS \
   -DUSE_CYRUS_SASL -I/usr/include/sasl'
 
+LIBS=$(echo /lib/*-linux-gnu*)
+USR_LIBS=$(echo /usr/lib/*-linux-gnu*)
+
 #export AUXLIBS="-L${PREFIX}/lib -Wl,-rpath,$PREFIX/lib -lldap -llber -lssl -lcrypto -lsasl2"
-export AUXLIBS="-L/usr/local/lib -lsasl2 -lssl -lcrypto"
-export AUXLIBS_LDAP="-L/usr/local/lib -lldap -L/usr/local/lib -llber"
+export AUXLIBS="-L${USR_LIBS}/sasl -lsasl2 -L${LIBS} -lssl -lcrypto"
+export AUXLIBS_LDAP="-L${LIBS} -lldap -llber"
+PREFIX=${DIR}/../build/snap/postfix
 
 make makefiles shared=no
 make
-#make non-interactive-package install_root=${PREFIX}
-make non-interactive-package
-
-PREFIX=${DIR}/../build/snap/postfix
+make non-interactive-package install_root=${PREFIX}
+#make non-interactive-package
 
 mkdir ${PREFIX}
 cp -r /* ${PREFIX}
