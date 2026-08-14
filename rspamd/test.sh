14bc75c6188dd907f020adeb8300974e38baea07
Boris Rybalkin <ribalkin@gmail.com>
2026-08-14 20:03:20 +0100

    Check for the directories rspamd actually ships
    The lua code is spread across lualib, rules and plugins rather than sitting in
    one directory, and those are the paths the service points the daemon at, so
    they are what the check looks for.

--- rspamd/test.sh
+++ rspamd/test.sh
@@ -5,5 +5,10 @@ RSPAMD=${DIR}/../build/snap/rspamd
 
 ${RSPAMD}/bin/rspamd.sh --version
 ${RSPAMD}/bin/rspamadm.sh --version
+${RSPAMD}/bin/rspamc.sh --version
+
 test -f ${RSPAMD}/etc/rspamd.conf
-test -d ${RSPAMD}/share/lua
+test -d ${RSPAMD}/share/lualib
+test -d ${RSPAMD}/share/rules
+test -d ${RSPAMD}/share/plugins
+test -d ${RSPAMD}/share/www
