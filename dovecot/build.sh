ca21d7d48ff05a30c70dea21cc18513095464da4
Boris Rybalkin <ribalkin@gmail.com>
2026-08-16 03:57:53 +0100

    Give dovecot the compression library it now insists on
    The new version will not configure without zlib, where the old one did not ask
    for it at all.

--- dovecot/build.sh
+++ dovecot/build.sh
@@ -12,7 +12,7 @@ PREFIX=/snap/mail/current/${NAME}
 OUTPUT=${DIR}/../build/snap/${NAME}
 
 apt-get update
-apt-get -y install build-essential libncurses5-dev libldap2-dev libsasl2-dev libssl-dev libldb-dev wget
+apt-get -y install build-essential libncurses5-dev libldap2-dev libsasl2-dev libssl-dev libldb-dev zlib1g-dev pkg-config wget
 
 rm -rf ${PREFIX}
 mkdir -p ${PREFIX}
